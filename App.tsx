ff27b1c67411a9db9d02ab559bd5839fd0330530
Aniket Pandharabale <aniketicloud@gmail.com>
2022-05-04 23:02:48 +0530

    styling the number input element

--- App.tsx
+++ App.tsx
@@ -1,28 +1,10 @@
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 import { StartGameScreen } from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <View style={styles.inputContainer}>
+    <View>
       <StartGameScreen />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  inputContainer: {
-    backgroundColor: "#72063c",
-    padding: 16,
-    marginHorizontal: 24,
-    marginTop: 100,
-    borderRadius: 8,
-    elevation: 4,
-    shadowColor: "black",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowRadius: 6,
-    shadowOpacity: 0.25,
-  },
-});
--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,12 +1,41 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import { PrimaryButton } from "../components/PrimaryButton";
 
 export const StartGameScreen = () => {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.textInput} maxLength={2} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    backgroundColor: "#72063c",
+    padding: 16,
+    marginHorizontal: 24,
+    marginTop: 100,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  textInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
