d7201526386735566258f8da1f0fd67d55116bd0
Aniket Pandharabale <aniketicloud@gmail.com>
2022-06-28 20:11:04 +0530

    Introducing the Dimensions API

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { FlatList, View, StyleSheet, Alert, Text } from "react-native";
+import { FlatList, View, StyleSheet, Alert } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 import { NumberContainer } from "../components/game/NumberContainer";
